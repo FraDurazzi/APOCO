e0fa99f1de118af0e812a44319bc9a761a34a641
Francesco Durazzi <fra.durazzi@gmail.com>
2020-03-18 14:16:58 +0100

    clean

--- sh/daily_update.sh
+++
@@ -1,4 +0,0 @@
-/home/PERSONALE/francesco.durazzi2/anaconda3/bin/jupyter nbconvert \
-                      --execute \
-                      --to notebook /home/PERSONALE/francesco.durazzi2/projects/APOCO/APOCO/notebooks/SIR.ipynb \
-                      --output /home/PERSONALE/francesco.durazzi2/projects/APOCO/APOCO/SIR_output.ipynb
